c7b2ddf694b9423eefebba4780651f4c1056abbc
thosaphol <thosaphol@outlook.co.th>
2025-04-27 15:16:30 +0700

    update case

--- TestCalculator/UnitTest1.cs
+++ TestCalculator/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace TestCalculator
         [InlineData("Test Insert 1", 1, 2, 3)]
         [InlineData("Test Insert 2", 4, 9, 13)]
         [InlineData("Test Insert 3", 3, 5, 8)]
-        public void Test1(string testcase, int left, int right, int expect)
+        public void Test1(string testcasename, int left, int right, int expect)
         {
             var calculator = new Calculator();
             if (calculator.Sum(left, right) != expect)
